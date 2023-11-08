562f484d3f9b7e0a5df8e2bd9d49ee976fbf49e9
Prashant_Chaturvedi <prashant1234chh@gmail.com>
2023-11-08 23:54:54 +0530

    express setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import express from 'express';
+const app = express();
 
 dotenv.config({
     path: './env'
 })
 
 connectDB();
+
+app.listen(8000, () => {
+    console.log('server started');
+})
 
 
 
